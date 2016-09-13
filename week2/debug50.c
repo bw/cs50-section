dd6e2e5709cd67bbca16e2702c3eb392291237b1
Brandon Wang <git@brandon.wang>
2016-09-13 02:45:31 -0400

    Update debug50.c

--- week2/debug50.c
+++ week2/debug50.c
@@ -1,28 +1,28 @@
 #include <cs50.h>
 #include <stdio.h>
 
-// a lack of comments makes this harder to debug!
-int main(void)
-{
-    float f;
+int main(void) {
+    int a;
+    int b;
+    int c;
+    
+    a = 5;
+    b = a + 2;
+    c = a + b;
+    a = b - c;
 
-    // set a breakpoint here, then make debug, debug50 ./debug
-    do
-    {
+    do {
         printf("How much change do you want? ");
         f = get_float();
-    }
-    while(f < 0);
+    } while (f < 0);
 
     int c = f * 100;
 
-    while(true)
-    {
+    while (true) {
         ;
     }
 
     printf("%i\n", c);
 
     return 0;
-
 }
