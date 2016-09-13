b90a81c34bfd7e05abdc984ea3839eff43615cf5
Brandon Wang <git@brandon.wang>
2016-09-13 02:43:53 -0400

    Create arrays.c

---
+++ week2/arrays.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void) {
+    // Make an array that is your name
+    
+    // Iterate over its members
+    
+    // Give me corresponding ASCII integers for each character
+    
+}
